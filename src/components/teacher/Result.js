45d54f165f9c443b1030ff8450e110dc6135d788
Spencer Hamilton <hamiltons@mdsd.org>
2020-10-21 10:20:23 -0600

    indv result page

--- src/components/teacher/Result.js
+++ src/components/teacher/Result.js
@@ -1,18 +1,22 @@
 import React, {useState, useEffect, useRef} from 'react'
-import {useLocation} from 'react-router-dom'
+import {useLocation, Link} from 'react-router-dom'
 import axios from 'axios'
 import Spinner from '../Spinner'
 const Result = ({createAlert}) => {
     const token = useRef(localStorage.getItem('token'))
     const [loading, setLoading] = useState(true)
     const [resultdata, setResultdata] = useState([])
+    const [testName, setTestName] = useState('');
+    const [student, setStudent] = useState({})
     const [result, setResult] = useState({})
+    
     function useQuery() {
         return new URLSearchParams(useLocation().search);
       }
       const query = useQuery();
       let result_id = query.get('result_id')
       useEffect(() => {
+        let parse = require('postgres-date')
           setLoading(true)
         axios
         .get(
@@ -24,9 +28,11 @@ const Result = ({createAlert}) => {
           }
         )
         .then((res) => {
-          setLoading(false);
           setResultdata(res.data.resultdata);
           setResult(res.data.result)
+          setStudent(res.data.student);
+          setTestName(res.data.test_name)
+            setLoading(false);
         })
         .catch(() => {
           createAlert(
@@ -46,14 +52,39 @@ const Result = ({createAlert}) => {
     Student Result
   </div>
   <div className="card-body">
-    <h5 className="card-title">{result.student_id}</h5>
-    <p className="card-text">Test: {result.test_id}<br />Score: 10/10</p>
-    <button className="btn btn-primary">Go back</button>
+    <h5 className="card-title">{student.first_name} {student.last_name}</h5>
+    <p>Username: {student.username}</p>
+    <p className="card-text">Test: {testName}<br />Score: 10/10</p>
+    <Link to='/teacher/results' className="btn btn-primary">Go back</Link>
   </div>
   <div className="card-footer text-muted">
-    Time test was taken
+    {/* {result.created_at} TIME */}
   </div>
 </div>
+<table className="table table-striped">
+<thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Correct Answer</th>
+      <th scope="col">Student Answer</th>
+    </tr>
+  </thead>
+  <tbody>
+    {resultdata.map(ans => (<tr style={{color: `${ans.correct ? 'green' : 'red'}`}}><th scope="row">{ans.line_number}</th>
+    <td>{ans.word}</td>
+    <td>{ans.answer}</td></tr>))}
+    <tr style={{color: 'red'}}>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+    </tr>
+  </tbody>
+</table>
         </div>
     )
 }
