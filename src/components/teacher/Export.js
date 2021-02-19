3049a5f3637dc5fb6e7a3b59114725ea05ab91df
spencer119 <spencernhamilton@gmail.com>
2021-02-18 23:02:17 -0700

    cleaned up csv headers. params next

--- src/components/teacher/Export.js
+++ src/components/teacher/Export.js
@@ -3,9 +3,12 @@ import axios from 'axios';
 
 const Export = ({ createAlert }) => {
   let fileDownload = require('js-file-download');
+  const [downloading, setDownloading] = useState(false);
   const token = useRef(localStorage.getItem('token'));
 
   const getData = (params) => {
+    if (downloading) return;
+    setDownloading(true);
     axios
       .get(
         process.env.NODE_ENV === 'development'
@@ -17,9 +20,11 @@ const Export = ({ createAlert }) => {
       )
       .then((res) => {
         fileDownload(res.data, 'export.csv');
+        setDownloading(false);
       })
       .catch(() => {
         createAlert('There was an error exporting the data. Please try again.', 'danger', 5000);
+        setDownloading(false);
       });
   };
   return (
