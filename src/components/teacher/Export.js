a5d9b85d3cdf7388b38fa90c2230f2030ed65998
spencer119 <spencernhamilton@gmail.com>
2021-02-18 22:49:59 -0700

    export works. Clean up look next

--- src/components/teacher/Export.js
+++ src/components/teacher/Export.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import axios from 'axios';
 
-const Export = () => {
-  return <div className='container'>Export</div>;
+const Export = ({ createAlert }) => {
+  let fileDownload = require('js-file-download');
+  const token = useRef(localStorage.getItem('token'));
+
+  const getData = (params) => {
+    axios
+      .get(
+        process.env.NODE_ENV === 'development'
+          ? '/api/v2/teacher/export'
+          : 'https://spelling-tests-backend.herokuapp.com/api/v2/teacher/export',
+        {
+          headers: { token: token.current },
+        }
+      )
+      .then((res) => {
+        fileDownload(res.data, 'export.csv');
+      })
+      .catch(() => {
+        createAlert('There was an error exporting the data. Please try again.', 'danger', 5000);
+      });
+  };
+  return (
+    <div className='container'>
+      Export <button onClick={getData}>GET</button>
+    </div>
+  );
 };
 
 export default Export;
